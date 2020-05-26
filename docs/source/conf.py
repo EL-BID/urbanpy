1543f8ddc2ca2c80f15fec3a9afd541c8fe8b866
Claudio Ortega <claudio.rtega2701@gmail.com>
2020-05-26 12:55:16 -0500

    change alabaster theme for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../urbanpy/plotting'))
 # sys.path.insert(0, os.path.abspath('../../urbanpy/routing'))
 # sys.path.insert(0, os.path.abspath('../../urbanpy/utils'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -38,6 +39,7 @@ release = '0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
     'numpydoc'
 ]
 
@@ -55,7 +57,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
