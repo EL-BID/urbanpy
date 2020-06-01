d2fafe7eeaf0370379959abf29217556c34c3e1c
a-regal <andresregal97@gmail.com>
2020-06-01 18:17:04 -0500

    Finish unit tests for utils module. Deprecate tuples_to_lists.

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -54,19 +54,65 @@ class UtilTest(unittest.TestCase):
         except Exception as err:
             self.assertEqual(type(err), ValueError)
 
-    def test_t2l(self):
+    def test_shell_from_geometry(self):
         '''
-        Tests tuples to list function.
-
-        * Test within a JSON
-        * Test using __geo_interface__
-
+        Test conversion from array of dicts to list of coordinates
         '''
 
-        json = {'type': 'FeatureCollection', 'features': [{'id': 0, 'geometry': {'coordinates': (((-77, -12), (-77.1, -12.1)))}}]}
+        #Sample polygon
+        inp = [{'lat': -12.1057161, 'lon': -77.0453355},
+               {'lat': -12.1046923, 'lon': -77.0451272},
+               {'lat': -12.1048769, 'lon': -77.0442775},
+               {'lat': -12.1059175, 'lon': -77.0445076},
+               {'lat': -12.1057161, 'lon': -77.0453355}]
 
-        expected_json = {'type': 'FeatureCollection', 'features': [{'id': 0, 'geometry': {'coordinates': [[[-77, -12], [-77.1, -12.1]]]}}]}
+        self.assertEqual(up.utils.shell_from_geometry(inp), [[-77.0453355, -12.1057161],
+                                                             [-77.0451272, -12.1046923],
+                                                             [-77.0442775, -12.1048769],
+                                                             [-77.0445076, -12.1059175],
+                                                             [-77.0453355, -12.1057161]])
 
-        self.assertEqual(up.utils.tuples_to_lists(json), expected_json)
+    def test_duration_labels(self):
+        '''
+        Test the custom duration label creation with random integer sampling.
+
+        * Test from 0-10
+        * Test from 0-20
+        * Test from 10-100
+
+        '''
+        inp_1 = np.array([ 1.47499837,  4.44655595,  6.58557897,  8.96218615,  9.21594168])
+        inp_2 = np.array([ 7.28246721, 11.57711221,  2.3046021 , 16.37945587, 14.53965395])
+        inp_3 = np.array([38.78371215, 91.68736876, 51.42509596, 35.00861296, 52.06796523])
+
+        #Case 0-10
+        bins, test = up.utils.create_duration_labels(inp_1)
+        self.assertEqual(bins, [0, 15])
+        self.assertEqual(test, ['De 0 a 15'])
+
+        #Case 0-20
+        bins, test = up.utils.create_duration_labels(inp_2)
+        self.assertEqual(bins, [0, 15, 30])
+        self.assertEqual(test, ['De 0 a 15', 'De 15 a 30'])
+
+        #Case 10-100
+        bins, test = up.utils.create_duration_labels(inp_3)
+        self.assertEqual(bins, [0, 15, 30, 45, 60, 90, 120])
+        self.assertEqual(test, ['De 0 a 15', 'De 15 a 30', 'De 30 a 45', 'De 45 a 60', 'De 60 a 90', 'De 90 a 120'])
+
+    # def test_t2l(self):
+    #     '''
+    #     Tests tuples to list function.
+    #
+    #     * Test within a JSON
+    #     * Test using __geo_interface__
+    #
+    #     '''
+    #
+    #     json = {'type': 'FeatureCollection', 'features': [{'id': 0, 'geometry': {'coordinates': (((-77, -12), (-77.1, -12.1)))}}]}
+    #
+    #     expected_json = {'type': 'FeatureCollection', 'features': [{'id': 0, 'geometry': {'coordinates': [[[-77, -12], [-77.1, -12.1]]]}}]}
+    #
+    #     self.assertEqual(up.utils.tuples_to_lists(json), expected_json)
 if __name__ == '__main__':
     unittest.main()
--- urbanpy/plotting/plotting.py
+++ urbanpy/plotting/plotting.py
@@ -2,78 +2,14 @@ import pydeck as pdk
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
-from urbanpy.utils import tuples_to_lists
+#from urbanpy.utils import tuples_to_lists
 
 __all__ = [
-    'pydeck_df',
+    #'pydeck_df',
     'gen_pydeck_layer',
     'choropleth_map',
 ]
 
-def pydeck_df(gdf, features, cmap, bins, color_feature):
-    '''
-    Prepare a DataFrame for Polygon plotting in PyDeck
-
-    Parameters
-    ----------
-
-    gdf : GeoDataFrame with the geometries to plot
-
-    features : list
-                  List of features to add to the polygon df
-
-    cmap : str
-              Matplotlib colormap to use for plotting
-
-    bins : list
-              Bins to aggregate data into
-
-    color_feature : str
-                       Column name for data to transform into bins and color features
-
-    Returns
-    -------
-
-    polygon_df : DataFrame
-                    df with the coordinates in a list as a column and the selected features
-
-    '''
-
-    cmap = plt.get_cmap(name='magma')
-    json = gdf.__geo_interface__
-
-    json_ = tuples_to_lists(json)
-
-    del json_['bbox']
-
-    df = pd.DataFrame.from_dict(json_)
-
-    polygon_df = pd.DataFrame()
-    polygon_df['coordinates'] = df['features'].apply(lambda row: row['geometry']['coordinates'])
-
-    for feature in features:
-        polygon_df[feature] = df['features'].apply(lambda row: row['properties'][feature])
-
-    polygon_df['bins'] = pd.cut(
-        polygon_df[color_feature],
-        bins = bins
-    )
-
-    bins_labels = polygon_df['bins'].unique().categories.values
-    bins_replace = {label: i  for i, label in enumerate(bins_labels)}
-    polygon_df['count'] = polygon_df['bins'].replace(bins_replace)
-
-    cmap_ixs = [int(round(255 / (i+1))) for i in polygon_df['count'].fillna(0).values]
-    rgb_values = [[color * 255 for color in cmap.colors[cmap_ix]] for cmap_ix in cmap_ixs]
-    rgb_count_df = pd.DataFrame(rgb_values, columns=['r','g','b'])
-
-    polygon_df = pd.concat((polygon_df, rgb_count_df), axis=1)
-
-    polygon_df['r'] = polygon_df['r'].round(0)
-    polygon_df['g'] = polygon_df['g'].round(0)
-    polygon_df['b'] = polygon_df['b'].round(0)
-
-    return polygon_df
 
 def gen_pydeck_layer(layer_type, data, **kwargs):
     if layer_type == 'H3HexagonLayer':
@@ -163,3 +99,68 @@ def scatterplot(df, x, y, engine, save, filename=None, **kwargs):
 
     else:
         pass
+
+# def pydeck_df(gdf, features, cmap, bins, color_feature):
+#     '''
+#     Prepare a DataFrame for Polygon plotting in PyDeck
+#
+#     Parameters
+#     ----------
+#
+#     gdf : GeoDataFrame with the geometries to plot
+#
+#     features : list
+#                   List of features to add to the polygon df
+#
+#     cmap : str
+#               Matplotlib colormap to use for plotting
+#
+#     bins : list
+#               Bins to aggregate data into
+#
+#     color_feature : str
+#                        Column name for data to transform into bins and color features
+#
+#     Returns
+#     -------
+#
+#     polygon_df : DataFrame
+#                     df with the coordinates in a list as a column and the selected features
+#
+#     '''
+#
+#     cmap = plt.get_cmap(name='magma')
+#     json = gdf.__geo_interface__
+#
+#     json_ = tuples_to_lists(json)
+#
+#     del json_['bbox']
+#
+#     df = pd.DataFrame.from_dict(json_)
+#
+#     polygon_df = pd.DataFrame()
+#     polygon_df['coordinates'] = df['features'].apply(lambda row: row['geometry']['coordinates'])
+#
+#     for feature in features:
+#         polygon_df[feature] = df['features'].apply(lambda row: row['properties'][feature])
+#
+#     polygon_df['bins'] = pd.cut(
+#         polygon_df[color_feature],
+#         bins = bins
+#     )
+#
+#     bins_labels = polygon_df['bins'].unique().categories.values
+#     bins_replace = {label: i  for i, label in enumerate(bins_labels)}
+#     polygon_df['count'] = polygon_df['bins'].replace(bins_replace)
+#
+#     cmap_ixs = [int(round(255 / (i+1))) for i in polygon_df['count'].fillna(0).values]
+#     rgb_values = [[color * 255 for color in cmap.colors[cmap_ix]] for cmap_ix in cmap_ixs]
+#     rgb_count_df = pd.DataFrame(rgb_values, columns=['r','g','b'])
+#
+#     polygon_df = pd.concat((polygon_df, rgb_count_df), axis=1)
+#
+#     polygon_df['r'] = polygon_df['r'].round(0)
+#     polygon_df['g'] = polygon_df['g'].round(0)
+#     polygon_df['b'] = polygon_df['b'].round(0)
+#
+#     return polygon_df
